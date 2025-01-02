ae99d30daeee6f7c976f711e7a36d77262626190
Rodolfo Govedarski-Roldao <r_roldao@hotmail.com>
2025-01-02 18:33:06 +0200

    finish  main structure

--- main.py
+++ main.py
@@ -12,17 +12,18 @@ class GameBoard:
         }
 
     def print_board(self):
-        print(f"{self.board['a1']}|{self.board['a2']}|{self.board['a3']}")
+        print("    A  B   C")
+        print(f"1 {self.board['a1']}|{self.board['a2']}|{self.board['a3']}")
         print("------------")
-        print(f"{self.board['b1']}|{self.board['b2']}|{self.board['b3']}")
+        print(f"2 {self.board['b1']}|{self.board['b2']}|{self.board['b3']}")
         print("------------")
-        print(f"{self.board['c1']}|{self.board['c2']}|{self.board['c3']}")
+        print(f"3 {self.board['c1']}|{self.board['c2']}|{self.board['c3']}")
 
     def place_play(self, player, column, line):
         if player == "p_one":
-            self.board[f"{column}{line}"] = self.p_one
+            self.board[f"{column.lower()}{line}"] = self.p_one
         else:
-            self.board[f"{column}{line}"] = self.p_two
+            self.board[f"{column.lower()}{line}"] = self.p_two
 
     def verify_player(self, symbol):
         if symbol == " X ":
@@ -57,11 +58,11 @@ while game:
     game_board.print_board()
     player_one_column = input("Player One, which is the column? A, B, C?")
     player_one_line = input("And what is the line?1, 2 or 3?")
-    game_board.place_play("p_one", player_one_column, player_one_line)
+    game_board.place_play("p_one", player_one_column.lower(), player_one_line)
     game_board.print_board()
     player_two_column = input("Player Two, which is the column? A, B, C?")
     player_two_line = input("And what is the line?1, 2 or 3?")
-    game_board.place_play("p_two", player_two_column, player_two_line)
+    game_board.place_play("p_two", player_two_column.lower(), player_two_line)
     verify_game = game_board.verify_game()
     if verify_game[0]:
         print(verify_game[2])
